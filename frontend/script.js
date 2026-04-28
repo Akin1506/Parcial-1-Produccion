03ff2859f7d1695c0bf1dc6ffdfadde567189bd9
akin-15 <haquin97@gmail.com>
2026-04-27 23:19:26 -0400

    Version produccion con Render y Vercel

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-const API_URL = "http://127.0.0.1:8000";
+const API_URL = "https://parcial-12d26.onrender.com/";
 
 const loginView = document.getElementById("login-view");
 const appView = document.getElementById("app-view");
